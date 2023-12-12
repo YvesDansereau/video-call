a451361cae70cd0ff25326e4e390665b7034e04b
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-12-12 11:13:14 +0900

    add local video

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,14 +21,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _localVideoRenderer = RTCVideoRenderer();
+
+  Future<void> _initializeLocalVideoRenderer() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': {
+        'facingMode': 'user',
+      },
+    };
+    MediaStream stream =
+        await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+    await _localVideoRenderer.initialize();
+    _localVideoRenderer.srcObject = stream;
+  }
+
+  @override
+  void initState() {
+    _initializeLocalVideoRenderer();
+    super.initState();
+  }
+
+  @override
+  Future<void> dispose() async {
+    await _localVideoRenderer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ビデオ通話'),),
-      body: const Text('test'),
+      appBar: AppBar(
+        title: const Text('ビデオ通話'),
+      ),
+      body: RTCVideoView(_localVideoRenderer, mirror: true,),
     );
   }
 }
